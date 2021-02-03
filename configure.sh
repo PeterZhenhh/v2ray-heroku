794c60a24e0496df154879c1e22144adaea4032f
PeterZhenhh <2306289410@qq.com>
2021-02-03 19:43:35 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -16,7 +16,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 {
     "inbounds": [
         {
-            "port": 80,
+            "port": 8081,
             "protocol": "vmess",
             "settings": {
                 "clients": [
